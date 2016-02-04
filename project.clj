52e942869bc24289c80a23e6351c41755abb264f
Marcos <marcoslamuria@gmail.com>
2016-02-04 15:51:19 +0000

    change lib name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject file-management "0.1.0"
+(defproject clanhr/file-management "0.2.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
