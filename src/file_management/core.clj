2036c92e9516bc7f29317b8eb3508bc9fbc81805
Marcos <marcoslamuria@gmail.com>
2016-02-12 09:45:39 +0000

    decode avatar from base64 and add util functions to get avatar url and build avatar key

--- src/file_management/core.clj
+++ src/file_management/core.clj
@@ -1,9 +1,22 @@
 (ns file-management.core
+  (use clojure.java.io)
+  (:import (org.apache.commons.codec.binary Base64))
   (:require [aws.sdk.s3 :as s3]))
 
 (defn build-avatar-key
   [user-id]
   (str user-id "-avatar"))
+
+(defn get-avatar-url
+  [credentials user-id]
+  (str "https://" (:bucket credentials) ".s3.amazonaws.com/" (build-avatar-key user-id)))
+
+(defn get-img-stream
+  [src]
+  (let [src-parsed (clojure.string/replace src #"^data:image/png;base64," "")
+        data (Base64/decodeBase64 src-parsed)]
+    (input-stream data)))
+
 
 (defn put-user-avatar
   [credentials user-id avatar-value]
@@ -12,7 +25,10 @@
         (some? (:bucket credentials))
         (some? user-id)
         (some? avatar-value)}
-  (s3/put-object credentials
-                 (:bucket credentials)
-                 (build-avatar-key user-id)
-                 avatar-value))
+    (s3/put-object credentials
+                   (:bucket credentials)
+                   (build-avatar-key user-id)
+                   (get-img-stream avatar-value)
+                   {:content-type "image/png"}
+                   (s3/grant :all-users :read))
+    (get-avatar-url credentials user-id))
