4cf9ad275adfb701711b838cdb18ae4202e39e30
Marcos <marcoslamuria@gmail.com>
2016-03-31 17:25:44 +0100

    Simplify the function interface to upload files
    Now you don't need to call the put-file function with the file-key
    option. So now we have the following 2 options to call this function:
    
    (file-management/put-file credentials-hash file-key file-info)
    (file-management/put-file credentials-hash file-info)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clanhr/file-management "0.7.0"
+(defproject clanhr/file-management "0.7.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
