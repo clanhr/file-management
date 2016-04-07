69ab5fc7e8d3fa177122d263563c19c7bd1e9cd3
Marcos <marcoslamuria@gmail.com>
2016-04-07 14:00:02 +0100

    Accept file in base64
    Did some refactoring to accept base64 files. To accomplish this I added
    2 functions:
     - get-content-type to get content type from base64 data
     - get-file-parsed to get content value from base64 data

--- test/file_management/core_test.clj
+++ test/file_management/core_test.clj
@@ -4,3 +4,20 @@
 
 (deftest build-avatar-key-test
   (is (= "test-avatar" (core/build-avatar-key "test"))))
+
+(deftest get-content-type-test
+  (is (= "image/png"
+         (core/get-content-type "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAZAAAAGQCAY")))
+  (is (= "application/pdf"
+         (core/get-content-type "data:application/pdf;base64,iVBORw0KGgoAAAANSUhEUgAAAZAAAAGQCAY")))
+  (is (= "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+         (core/get-content-type "data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,iVBORw0KGgoAAAANSUhEUgAAAZAAAAGQCAY"))))
+
+(deftest get-file-parsed-test
+  (let [content "iVBORw0KGgoAAAANSUhEUgAAAZAAAAGQCAY"]
+    (is (= content
+           (core/get-file-parsed (str "data:image/png;base64," content))))
+    (is (= content
+           (core/get-file-parsed (str "data:application/pdf;base64," content))))
+    (is (= content
+           (core/get-file-parsed (str "data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64," content))))))
