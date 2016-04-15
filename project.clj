8a773e58d4d32037be3e3ae8dd936f9950f558a4
Pedro Santos <donbonifacio@gmail.com>
2016-04-15 15:48:22 +0100

    Remove shared deps
    Motivation:
    
    Shared deps brings a heavy burden on a project. lein deps :tree takes
    60s with shared deps, and 3s without it. So we'll start using shared
    deps only on the services.

--- project.clj
+++ project.clj
@@ -1,16 +1,10 @@
-(defproject clanhr/file-management "0.7.3"
+(defproject clanhr/file-management "0.7.4"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
 
-  :dependencies.edn "https://raw.githubusercontent.com/clanhr/dependencies/master/dependencies.edn"
-
-  :dependency-sets [:clojure
-                    :common
-                    :clanhr]
-
-  :dependencies []
-
-  :plugins [[clanhr/shared-deps "0.2.6"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clanhr/result "0.11.0"]
+                 [clj-aws-s3 "0.3.10"]])
